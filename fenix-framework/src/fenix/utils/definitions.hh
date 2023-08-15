ea4a3b272c628f06e2aa9acc4aa822a330ca8fe1
eligum <eligum.djo@gmail.com>
2023-08-15 11:27:04 +0200

    fix: global variables specifiers

--- fenix-framework/src/fenix/utils/definitions.hh
+++ fenix-framework/src/fenix/utils/definitions.hh
@@ -6,21 +6,21 @@
 
 namespace fenix::def {
 
-    constexpr auto WINDOW_TITLE  = "FENIX - Engine";
-    constexpr auto WINDOW_WIDTH  = 1280u;
-    constexpr auto WINDOW_HEIGHT = 720u;
+    inline constexpr auto WINDOW_TITLE  = "FENIX - Engine";
+    inline constexpr auto WINDOW_WIDTH  = 1280u;
+    inline constexpr auto WINDOW_HEIGHT = 720u;
 
-    constexpr auto EDITOR_CAMERA_XZ_ANGLE       = -90.0f;
-    constexpr auto EDITOR_CAMERA_XY_ANGLE       = 0.0f;
-    constexpr auto EDITOR_CAMERA_DISTANCE       = 4.0f;
-    constexpr auto EDITOR_CAMERA_CENTER         = glm::vec3{0.0f, 0.0f, 0.0f};
-    constexpr auto EDITOR_CAMERA_UP             = glm::vec3{0.0f, 1.0f, 0.0f};
-    constexpr auto EDITOR_CAMERA_FOV            = 60.0f;
-    constexpr auto EDITOR_CAMERA_ASPECT_RATIO   = 16.0f / 9.0f;
-    constexpr auto EDITOR_CAMERA_Z_NEAR         = 0.1f;
-    constexpr auto EDITOR_CAMERA_Z_FAR          = 100.0f;
-    constexpr auto EDITOR_CAMERA_ROTATION_SPEED = 0.2f;
-    constexpr auto EDITOR_CAMERA_ZOOM_SPEED     = 0.2f;
-    constexpr auto EDITOR_CAMERA_PAN_SPEED      = 0.2f;
+    inline constexpr auto EDITOR_CAMERA_XZ_ANGLE       = -90.0f;
+    inline constexpr auto EDITOR_CAMERA_XY_ANGLE       = 0.0f;
+    inline constexpr auto EDITOR_CAMERA_DISTANCE       = 4.0f;
+    inline constexpr auto EDITOR_CAMERA_CENTER         = glm::vec3{0.0f, 0.0f, 0.0f};
+    inline constexpr auto EDITOR_CAMERA_UP             = glm::vec3{0.0f, 1.0f, 0.0f};
+    inline constexpr auto EDITOR_CAMERA_FOV            = 60.0f;
+    inline constexpr auto EDITOR_CAMERA_ASPECT_RATIO   = 16.0f / 9.0f;
+    inline constexpr auto EDITOR_CAMERA_Z_NEAR         = 0.1f;
+    inline constexpr auto EDITOR_CAMERA_Z_FAR          = 100.0f;
+    inline constexpr auto EDITOR_CAMERA_ROTATION_SPEED = 0.2f;
+    inline constexpr auto EDITOR_CAMERA_ZOOM_SPEED     = 0.2f;
+    inline constexpr auto EDITOR_CAMERA_PAN_SPEED      = 0.2f;
 
 } // namespace fenix::def
